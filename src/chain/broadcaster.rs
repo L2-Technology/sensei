b6f2b9a33a241f03377b20bb5a916ef3d6b1ffea
John Cantrell <johncantrell97@gmail.com>
2022-04-02 14:34:54 -0400

    cleanup BitcoindClient usage

--- src/chain/broadcaster.rs
+++ src/chain/broadcaster.rs
@@ -3,16 +3,16 @@ use std::sync::Arc;
 use bitcoin::Transaction;
 use lightning::chain::chaininterface::BroadcasterInterface;
 
-use super::{bitcoind_client::BitcoindClient, listener_database::ListenerDatabase};
+use super::{listener_database::ListenerDatabase};
 
 pub struct SenseiBroadcaster {
-    pub bitcoind_client: Arc<BitcoindClient>,
+    pub broadcaster: Arc<dyn BroadcasterInterface + Send + Sync>,
     pub listener_database: ListenerDatabase,
 }
 
 impl BroadcasterInterface for SenseiBroadcaster {
     fn broadcast_transaction(&self, tx: &Transaction) {
-        self.bitcoind_client.broadcast_transaction(tx);
+        self.broadcaster.broadcast_transaction(tx);
 
         // TODO: there's a bug here if the broadcast fails
         //       best solution is to probably setup a zmq listener
--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -1,5 +1,6 @@
 pub mod bitcoind_client;
 pub mod broadcaster;
+pub mod fee_estimator;
 pub mod listener;
 pub mod listener_database;
 pub mod manager;
