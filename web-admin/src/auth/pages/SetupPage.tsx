cd8c171af859a4bd5b6c0907f9dca05694aa8eb6
Sam Makhlouf <maksam7dev@gmail.com>
2022-04-09 11:23:09 +0100

    styling setup and login inputs

--- web-admin/src/auth/pages/SetupPage.tsx
+++ web-admin/src/auth/pages/SetupPage.tsx
@@ -26,7 +26,7 @@ const UsernamePassphraseStep = () => {
   return (
     <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-plum-100 text-light-plum py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="bg-plum-100 text-light-plum py-8 px-4 shadow sm:rounded-xl sm:px-10">
           <div className="sm:mx-auto sm:w-full sm:max-w-md">
               <img src={logo} alt="sensei logo" className="h-20 mx-auto" />
             
@@ -49,12 +49,13 @@ const UsernamePassphraseStep = () => {
               </label>
               <div className="mt-1">
                 <input
+                autoFocus
                   id="username"
                   name="username"
                   type="text"
                   placeholder="admin"
                   required
-                  className="bg-plum text-light-plum appearance-none block w-full px-3 py-2 border border-plum-200 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="input"
                 />
               </div>
             </div>
@@ -72,7 +73,7 @@ const UsernamePassphraseStep = () => {
                   type="text"
                   placeholder="Satoshi"
                   required
-                  className="bg-plum text-light-plum appearance-none block w-full px-3 py-2 border border-plum-200 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="input"
                 />
               </div>
             </div>
@@ -89,14 +90,14 @@ const UsernamePassphraseStep = () => {
                   name="passphrase"
                   type="password"
                   required
-                  className="bg-plum text-light-plum appearance-none block w-full px-3 py-2 border border-plum-200 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="input"
                 />
               </div>
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange hover:bg-orange-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="btn-orange w-full justify-center"
               >
                 Setup
               </button>
