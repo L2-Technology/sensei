7a7e6f89e30e51297b41811fe87fba2da937e3b7
John Cantrell <johncantrell97@gmail.com>
2022-06-17 11:47:10 -0400

    fix incorrect navigation on pay invoice form

--- web-admin/src/payments/components/PayInvoiceForm.tsx
+++ web-admin/src/payments/components/PayInvoiceForm.tsx
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router";
 import payInvoice from "../mutations/payInvoice";
 import { Form, TextArea } from "../../components/form";
 import { z } from "zod";
+import { useQueryClient } from "react-query";
 
 export const CreateInvoiceInput = z.object({
   invoice: z.string(),
 });
 
 const PayInvoiceForm = () => {
+  const queryClient = useQueryClient();
   let navigate = useNavigate();
 
   return (
@@ -16,10 +18,12 @@ const PayInvoiceForm = () => {
       schema={CreateInvoiceInput}
       noticePosition="top"
       layout="default"
+      resetAfterSuccess={true}
       onSubmit={async ({ invoice }) => {
         try {
           await payInvoice(invoice);
-          navigate("/admin/payments");
+          queryClient.invalidateQueries("payments")
+          
         } catch (e) {
           // TODO: handle error
         }
